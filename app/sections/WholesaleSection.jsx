c0f232dd895489fd25be2079f9197a6d49b9ec9f
Fabian Ajokubi <favourajokubi@gmail.com>
2023-11-19 16:29:41 +0100

    add "Are you a wholesaler?"

---
+++ app/sections/WholesaleSection.jsx
@@ -0,0 +1,8 @@
+export default function WholesaleSection() {
+    return (
+        <section className="sm:grid place-items-center sm:pb-16 sm:px-32 pt-16 pb-16 px-12 text-center bg-primary-lightest/10 lg:text-2xl text-lg">
+            <span className="lg:text-3xl">Are you a <span className="text-primary-soft">wholesaler</span>?</span>
+            <span className="opacity-70">We accept assignment contracts.</span>
+        </section>
+    );
+}
