ee3acb254b3d94a93ac41fb6634ef1be3f04fb48
Fabian Ajokubi <favourajokubi@gmail.com>
2023-11-20 18:45:18 +0100

    Add br to WholesaleSection

--- app/sections/WholesaleSection.jsx
+++ app/sections/WholesaleSection.jsx
@@ -2,6 +2,7 @@ export default function WholesaleSection() {
     return (
         <section className="sm:grid place-items-center sm:pb-16 sm:px-32 pt-16 pb-16 px-12 text-center bg-primary-lightest/10 lg:text-2xl text-lg">
             <span className="lg:text-3xl">Are you a <span className="text-primary-soft">wholesaler</span>?</span>
+            <br />
             <span className="opacity-70">We accept assignment contracts.</span>
         </section>
     );
